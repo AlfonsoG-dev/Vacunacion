77df0c985ccd60ad63715db62d8199853b192015
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-14 23:42:43 -0500

    dejo el test

--- src/Mundo/prueba.java
+++ src/Mundo/prueba.java
@@ -3,7 +3,7 @@ package Mundo;
 public class prueba {
     public static void main(String[] args) {
         Cita mia = new Cita(112, "Enero-20.2021", 120, "Pasto");
-        Cita mia1 = new Cita(112, "Enero-20.2021", 120, "Pasto");
+        Cita mia1 = new Cita(113, "Enero-20.2021", 121, "Pasto");
         Usuario mio = new Usuario(192, "Cedula", "Alfonso", "Gomajoa", 3163, "@gmail", "Calle 2", mia);
         Usuario mio1 = new Usuario(193, "Cedula", "Sebastian", "Gomajoa", 3163, "@gmail", "Calle 2", mia1);
         Operacion miOperacion = new Operacion();
