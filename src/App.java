4a346bf011e039f257b4e414a5a81c6a74206ef3
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-15 00:52:13 -0500

    cambios para el funcionamiento de la interfaz

---
+++ src/App.java
@@ -0,0 +1,29 @@
+
+import javax.swing.JOptionPane;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+ /**
+  * Clase grafica con java fx
+  */
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) { 
+        Parent root;
+        try {
+            root= FXMLLoader.load(getClass().getResource("PanelLogin.fxml"));    
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Loggin");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al intentar llamar la interfaz: " + e.getMessage());
+        }
+    }
+ 
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/Interfaz/panelIngreso.java
+++ src/Interfaz/panelIngreso.java
@@ -1,4 +1,5 @@
 package Interfaz;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
