c7fc93a782fce137d031574c2e9a4b57aaf28ffd
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-15 00:31:15 -0500

    se crea la interfaz con javaFX

---
+++ src/Interfaz/panelIngreso.java
@@ -0,0 +1,28 @@
+package Interfaz;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class panelIngreso {
+
+    @FXML
+    private Button btnIngresar;
+
+    @FXML
+    private Button btnRegistro;
+
+    @FXML
+    private Label lblPasword;
+
+    @FXML
+    private Label lblUsuarioL;
+
+    @FXML
+    private PasswordField txtPasword;
+
+    @FXML
+    private TextField txtUsuario;
+
+}
