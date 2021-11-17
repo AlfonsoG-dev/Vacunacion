cfe94aace62b8956a61e8a55226b520312712096
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-17 08:50:19 -0500

    cambia el controlador para jdbc

--- src/Conexion/Conectar.java
+++ src/Conexion/Conectar.java
@@ -1,11 +1,37 @@
 package Conexion;
+import java.sql.Connection;
 
 public class Conectar {
     //-----------------------------//
     //----------Atributos---------//
     //----------------------------//
     /**
-     * cargar el driver de jdbc
+     * direccion url de la conexion
      */
-    
+    private final static String url="jdbc:postgressql://localhost:5432/GestorP";
+    /**
+     * usuario de la base de datos
+     */
+    private final static String user = "postgres";
+    /**
+     * contrasenia de la base de datos
+     */
+    private final static String  password = "5x5W12";
+    /**
+     * driver para la conexion
+     */
+    private final static String driver = "";
+    //---------------------------//
+    //---------Metodos----------//
+    //--------------------------//
+    public Connection conectar(){
+        Connection c = null;
+        try{
+            Class.forName(driver);
+
+        }catch(Exception e){
+            System.out.println("Error" + e.getMessage());
+        }
+        return c;
+    }
 }
