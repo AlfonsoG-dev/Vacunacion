2d08e09b6254abd5358aa73152c1cf7e14c99a06
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-16 20:54:48 -0500

    controlador del panel cita

---
+++ src/panelCita.java
@@ -0,0 +1,84 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class panelCita {
+
+    @FXML
+    private Button btnConsultar;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private Button btnModificar;
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private DatePicker dtaFecha;
+
+    @FXML
+    private Label lblCita;
+
+    @FXML
+    private Label lblCodigo;
+
+    @FXML
+    private Label lblCodigo1;
+
+    @FXML
+    private Label lblLugar;
+
+    @FXML
+    private Label lblTurno;
+
+    @FXML
+    private TableView<?> tblCitas;
+
+    @FXML
+    private TextField txtCita;
+
+    @FXML
+    private TextField txtCodigo;
+
+    @FXML
+    private TextField txtLugar;
+
+    @FXML
+    private TextField txtTurno;
+
+    @FXML
+    void btnConsultarOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEliminarOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnModificarOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegistrarOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnVolverOnClicked(ActionEvent event) {
+
+    }
+
+}
