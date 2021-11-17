b58e5899d84920a0eb9fd457e3b2c649f017c7b7
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-16 21:52:19 -0500

    se quita la scena

--- src/panelIngreso.java
+++ src/panelIngreso.java
@@ -11,7 +11,6 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 public class panelIngreso {
-
     /**
      * clase de tipo operacion
      */
