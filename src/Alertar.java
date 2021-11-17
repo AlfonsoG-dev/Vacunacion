e110a827c93e0cde7f78a8bdb7d865c03e832262
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-16 19:58:15 -0500

    se crean los comentarios de la ventana de alertas

--- src/Alertar.java
+++ src/Alertar.java
@@ -7,6 +7,13 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Alertar {
+    /**
+     * crear la ventana para los mensajes de alertas
+     * <b> pre: </b> los elementos de la interfaz se encuentran inicializados
+     * <b> post: </b>  se crea la ventana de elertas
+     * @param titulo, es el titulo de la ventana. titulo != "" && titulo != null
+     * @param mensaje, es el mensaje de la ventana. mensaje != "" && mensaje != null
+     */
     public static void display(String titulo, String mensaje){
         Stage windows = new Stage();
         windows.initModality(Modality.APPLICATION_MODAL);
@@ -23,6 +30,5 @@ public class Alertar {
         Scene scene = new Scene(layout, 200, 150);
         windows.setScene(scene);
         windows.showAndWait();
-
     }    
 }
