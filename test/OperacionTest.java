e91255e34c41d655656be99dd84bc9d30b3267c7
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-18 10:04:06 -0500

    cita de usuario es de tipo string ahora

--- test/OperacionTest.java
+++ test/OperacionTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-import Mundo.Cuenta;
 import Mundo.Cita;
 import Mundo.Usuario;
 public class OperacionTest {
@@ -23,16 +22,11 @@ public class OperacionTest {
      */
     private Cita miCita;
     /**
-     * clase necesaria para las pruebas
-     */
-    private Cuenta miCuenta;
-    /**
      * Escenario 1: Crear un usuario con cita
      */
     public void scenario1(){
-        miCuenta = new Cuenta("Alfonso", "123");
         miCita = new Cita(20, "octubre/12/2018", 12, "Pasto");
-        miUsuario = new Usuario(456, "identificacion", "lucas", "jojoa", 320, "@hotmail", "calle-12", miCita);
+        miUsuario = new Usuario(456, "identificacion", "lucas", "jojoa", 320, "@hotmail", "calle-12", String.valueOf(miCita.getCodigo()));
     }
     /**
      * Prueba 1: se prueba los metodos de la clase prueba
@@ -43,17 +37,13 @@ public class OperacionTest {
     public void testOperacion(){
         scenario1();
         assertNotNull("No deberia ser null", miOperacion.buscarCita(miCita));
-        assertNotNull("No deberia ser null", miOperacion.buscarCuenta(miCuenta));
         assertNotNull("No deberia ser null", miOperacion.buscarUsuario(miUsuario));
-        assertEquals("Deberian eliminar la cuenta", true, miOperacion.eliminarCuenta(miCuenta.darUsuario()).booleanValue());
         assertEquals("Deberia eliminar la cita", true, miOperacion.cancelarCita(miCita));
 
         //registrar una nueva cuenta 
-        Cuenta nueva = new Cuenta("juan", "456");
-        assertNotNull("Deberia registrar la cuenta", miOperacion.registrarCuenta(nueva.darUsuario(), nueva.darPasword()));
         //registrar una nueva cita
         Cita c = new Cita(23, "noviembre/28/2021", 123, "cali");
-        Usuario u = new Usuario(895, "cedula", "jack", "pantoja", 318, "@google", "sector centro", c);
+        Usuario u = new Usuario(895, "cedula", "jack", "pantoja", 318, "@google", "sector centro", String.valueOf(c.getCodigo()));
         assertNotNull("Deberia registar la cita", miOperacion.registrarCita(c, u));
     }
     /**
@@ -64,7 +54,7 @@ public class OperacionTest {
     @Test
     public void testOperacion2(){
         Cita c = new Cita(140, "octubre/1/2021", 50, "bogota");
-        Usuario u= new Usuario(105, "cedula", "jakob", "mirama", 318, "@gmail", "villaflores", c);
+        Usuario u= new Usuario(105, "cedula", "jakob", "mirama", 318, "@gmail", "villaflores", String.valueOf(c.getCodigo()));
         Usuario buscado = miOperacion.buscarUsuario(u);
         Cita buscada = miOperacion.buscarCita(c);
         if(miOperacion.registrarCita(c, u)!=null){
