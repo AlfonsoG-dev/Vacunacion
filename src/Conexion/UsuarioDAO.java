046484431f5fefb249fdbe5466acd30ec2a488e3
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-18 10:06:58 -0500

    para el codigo de la cita se utiliza solamente el usuario luego se busca en la cita

--- src/Conexion/UsuarioDAO.java
+++ src/Conexion/UsuarioDAO.java
@@ -1,5 +1,4 @@
 package Conexion;
-import Mundo.Cita;
 import Mundo.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,10 +8,6 @@ public class UsuarioDAO {
      * Conexion con la base de datos
      */
     private Conectar miConexion = new Conectar();
-    /**
-     * cita del usuario
-     */
-    private CitaDAO miCitaDAO = new CitaDAO();
     /**
      * buscar el usuario por numero de documento
      * <b> pre: </b> la base de datos se encuentra inicializada
@@ -30,7 +25,7 @@ public class UsuarioDAO {
             pst.setString(1, nDocumento);
             ResultSet rs = pst.executeQuery();
             if(rs.next()){
-                u  = new Usuario(Integer.parseInt(rs.getString(1)), rs.getString(2), rs.getString(3), rs.getString(4), Integer.parseInt(rs.getString(5)), rs.getString(6), rs.getString(7), miCitaDAO.buscarCita(rs.getString(8)));
+                u  = new Usuario(Integer.parseInt(rs.getString(1)), rs.getString(2), rs.getString(3), rs.getString(4), Integer.parseInt(rs.getString(5)), rs.getString(6), rs.getString(7), rs.getString(8));
             }
         }catch(Exception e){
             System.out.print("Error: " + e.getMessage());
@@ -41,53 +36,21 @@ public class UsuarioDAO {
         return u;
     }
     /**
-     * buscar usuario dado el codigo de la cita
-     * <b> pre: </b> la base de datos se encuentra inicializada
-     * <b> post: </b> se busca el usuario dado el codigo de cita
-     * @param nCodigo, es el codigo de la cita a buscar. nCodigo != "" && nCodigo != null
-     * @return true si el usuario tiene cita, false de lo contrario
-     */
-    public Boolean usuarioPorCodigo(Cita nCodigo){
-        Boolean encontrado = false;
-        Connection mia = miConexion.conectar();
-        Usuario nuevo = null;
-        PreparedStatement pst = null;
-        try{    
-            String sql = "select * from usuario where codigo =?";
-            pst = mia.prepareStatement(sql);
-            pst.setString(8, String.valueOf(nCodigo.getCodigo()));
-            ResultSet rs = pst.executeQuery();
-            if(rs.next()){
-                nuevo  = new Usuario(Integer.parseInt(rs.getString(1)), rs.getString(2), rs.getString(3), rs.getString(4), Integer.parseInt(rs.getString(5)), rs.getString(6), rs.getString(7), miCitaDAO.buscarCita(rs.getString(8)));
-                encontrado = true;
-            }
-        }catch(Exception e){
-            System.out.print("Error: " + e.getMessage());
-        }finally{
-            miConexion.desconectar(mia);
-        }
-        return encontrado;
-    }
-    /**
      * buscar el codigo de la cita dado el usuario
      * <b> pre: </b> la base de datos se encuentra inicializada
      * <b> post: </b> se busca el codigo de la cita
      * @param documento, es el numero de documento del usuario. documento != null && documento != ""
      * @return el codigo de la cita
      */
-    public String codigoCitaUsuario(String documetno){
+    public String codigoCitaUsuario(String documento){
         String codigo = null;
+        Usuario buscar = null;
         try{
-
-            if(buscarUsuario(documetno)!=null){
-                if(buscarUsuario(documetno).getCita().getCodigo()!=0){
-                    codigo = String.valueOf(buscarUsuario(documetno).getCita().getCodigo());
-                }
-                else{
-                    codigo = "null";
-                }
+            buscar = buscarUsuario(documento);
+            if(buscar != null){
+                codigo = buscar.getCita();
             }else{
-                System.out.print("Error al consultar la cita");
+                System.out.print("El usuario no existe");
             }
         }catch(Exception e){
             System.out.print("Error: " + e.getMessage());
