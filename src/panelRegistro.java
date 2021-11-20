ab06e1470c23860132b50649ea3b28715258b4d0
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-20 01:15:39 -0500

    interfaz para el registro del usuario y la cita con controlador

---
+++ src/panelRegistro.java
@@ -0,0 +1,102 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class panelRegistro {
+
+    @FXML
+    private Button btnCncelar;
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    private Button btnRegistrar1;
+
+    @FXML
+    private ComboBox<?> cbxCodigoCita;
+
+    @FXML
+    private ComboBox<?> cbxDocumento;
+
+    @FXML
+    private ComboBox<?> cbxTipo;
+
+    @FXML
+    private DatePicker dtaFecha;
+
+    @FXML
+    private Label lblApellido;
+
+    @FXML
+    private Label lblCelular;
+
+    @FXML
+    private Label lblCodigoCita;
+
+    @FXML
+    private Label lblCorreo;
+
+    @FXML
+    private Label lblDireccion;
+
+    @FXML
+    private Label lblDocumento;
+
+    @FXML
+    private Label lblFechaCIta;
+
+    @FXML
+    private Label lblLugarICita;
+
+    @FXML
+    private Label lblNombre;
+
+    @FXML
+    private Label lblRegistroCita;
+
+    @FXML
+    private Label lblRegistroUsuario;
+
+    @FXML
+    private Label lblTipo;
+
+    @FXML
+    private Label lblTurnoCita;
+
+    @FXML
+    private TextField txtApellido;
+
+    @FXML
+    private TextField txtCelular;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private TextField txtLugar;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtTurno;
+
+    @FXML
+    void btnCancelarOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegistrarOnClicked(ActionEvent event) {
+
+    }
+
+}
