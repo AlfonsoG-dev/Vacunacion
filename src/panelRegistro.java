62ef7e862ddcbec16d9b58be8bb6cfffb7848452
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-11-20 02:05:42 -0500

    construccion de los metodos consulta, actualizar elementos de interfaz y limpiar elementos

--- src/panelRegistro.java
+++ src/panelRegistro.java
@@ -1,5 +1,7 @@
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -7,10 +9,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import Mundo.Cita;
 import Mundo.Usuario;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
 import Conexion.CitaDAO;
 import Conexion.UsuarioDAO;
 
-public class panelRegistro {
+public class panelRegistro implements Initializable{
     /**
      * 
      */
@@ -27,9 +34,6 @@ public class panelRegistro {
 
     @FXML
     private Button btnRegistrar1;
-
-    @FXML
-    private ComboBox<Integer> cbxCodigoCita;
 
     @FXML
     private ComboBox<Integer> cbxDocumento;
@@ -86,6 +90,9 @@ public class panelRegistro {
     private TextField txtCelular;
 
     @FXML
+    private TextField txtCodigo;
+
+    @FXML
     private TextField txtCorreo;
 
     @FXML
@@ -102,12 +109,85 @@ public class panelRegistro {
 
     @FXML
     void btnCancelarOnClicked(ActionEvent event) {
-
+        limpiar();
     }
 
     @FXML
     void btnRegistrarOnClicked(ActionEvent event) {
 
     }
+    @FXML
+    void btnVerificarOnClicked(ActionEvent event) {
+        String documento = String.valueOf(cbxDocumento.getSelectionModel().getSelectedItem());
+        if(!documento.isEmpty()){
+            Usuario nUsuario = usuarioDAO.buscarUsuario(documento);
+            String codigo = nUsuario.getCita();
+            Cita nCita = citaDAO.buscarCita(codigo); 
+            if(nCita != null && nUsuario != null){
+                actualizarElementos(nUsuario, nCita);
+            }else{
+                Alertar.display("Verificar", "El usuario o la cita estan mal¡¡??");
+            }
+        }else{
+            Alertar.display("Verificar", "Seleccione un usuario");
+        }
+    }
+    /**
+     * se inicializan los combo cox
+     * <b> pre: </b> los elementos se encuentran inicializados
+     * <b> post: </b> se inicializan la combo box
+     */
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1){
+        agregarElemento();
+    }
+    /**
+     * actualizar elementos de la interfaz con la informacion de usuario y cita
+     * <b> pre: </b> los elementos de la interfaz se encuentran inicializados
+     * <b> post: </b> se actualiza la informacion de los elementos
+     * @param nUsuario, es el usuario con la informacion. nUsuario != null && nUsuario != ""
+     * @param nCita, es la cita con la informacion. nCita != "" && nCita != null
+     */
+    public void actualizarElementos(Usuario nUsuario, Cita nCita){
+        txtApellido.setText(nUsuario.getApellido());
+        txtCelular.setText(String.valueOf(nUsuario.getCelular()));
+        txtCorreo.setText(nUsuario.getCorreo());
+        txtDireccion.setText(nUsuario.getDireccion());
+        txtLugar.setText(nCita.getLugar());
+        txtNombre.setText(nUsuario.getNombre());
+        txtTurno.setText(String.valueOf(nCita.getTurno()));
+        dtaFecha.setValue(LocalDate.parse(nCita.getFecha()));
+        txtCodigo.setText(String.valueOf(nCita.getCodigo()));
+    }
+    /**
+     * limpiar los elementos de la interfaz
+     * <b> pre: </b> los elementos de la interfaz se encuentran inicializados
+     * <b> post: </b> se limpian los elementos de la interfaz
+     */
+    public void limpiar(){
+        txtApellido.setText(null);
+        txtCelular.setText(null);
+        txtCorreo.setText(null);
+        txtDireccion.setText(null);
+        txtLugar.setText(null);
+        txtNombre.setText(null);
+        txtTurno.setText(null);
+        txtCodigo.setText(null);
+        dtaFecha.setValue(null);
+        cbxDocumento.setValue(null);
+        cbxTipo.setValue(null);
+    }
+    /**
+     * agregar elementos a las combo box
+     * <b> pre: </b> la lista de elementos se encuentra inicializada
+     * <b> post: </b> se agrega el elemento a la combo box
+     */
+    public void agregarElemento(){
+        ObservableList<Integer> usuarios = usuarioDAO.seleccionarUsuario();
+        cbxDocumento.setItems(usuarios);
+        cbxTipo.getItems().add("id");
+        cbxTipo.getItems().add("otro");
+        cbxTipo.getItems().add("cc");
 
+    }
 }
